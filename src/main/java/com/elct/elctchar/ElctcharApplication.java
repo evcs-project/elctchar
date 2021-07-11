1ba3b3fd7e9d91e8802e3f986148555059e2c495
saga <wlatmdqja@naver.com>
2021-07-12 03:35:09 +0900

    init

---
+++ src/main/java/com/elct/elctchar/ElctcharApplication.java
@@ -0,0 +1,13 @@
+package com.elct.elctchar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElctcharApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElctcharApplication.class, args);
+    }
+
+}
